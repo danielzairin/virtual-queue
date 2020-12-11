86b561b1ec6608ac0abfe1a9f9d0c9735b68ef5e
danielzairin <danielzairin@student.usm.my>
2020-12-12 00:14:43 +0800

    Fixed queuer/establishment undefined error

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
 
   return (
     <div>
-      {queuer === null && establishment === null ? (
+      {queuer === undefined || establishment === undefined ? (
         <p>Connecting to database...</p>
       ) : (
         <div>
