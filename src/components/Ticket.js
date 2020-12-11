33ab5615603b31ff722549a251327137d221d1ee
danielzairin <danielzairin@student.usm.my>
2020-12-11 22:52:04 +0800

    Teached Hilmi

--- src/components/Ticket.js
+++ src/components/Ticket.js
@@ -1,15 +1,28 @@
-import { useEffect, useContext } from "react";
+import { useEffect, useContext, useState } from "react";
 import { QueuerContext } from "../contexts/QueuerContext";
-// import { db } from "../firebase";
+import { db } from "../firebase";
 
 function Ticket() {
   const queuer = useContext(QueuerContext);
-  // const [establishment, setEstablishment] = useState();
+  const [establishment, setEstablishment] = useState(null);
 
   useEffect(() => {
     // 1. If queuer's status is NOT idle
-    // 1.1 Listen to the establishment the queuer is queueing for
+    if (queuer.status !== "idle") {
+      // 1.1 Listen to the establishment the queuer is queueing for
+      db.collection("establishments")
+        .doc(queuer.queueingFor)
+        .onSnapshot((doc) => {
+          setEstablishment(doc.data());
+        });
+    }
   }, []);
+
+  // Statuses
+  // 1. idle
+  // 2. queueing
+  // 3. allowed
+  // 4. denied
 
   return (
     <div>
@@ -18,8 +31,17 @@ function Ticket() {
       <p>Status: {queuer.status}</p>
       <p>Queueing for: {queuer.queueingFor}</p>
       {/* Render ticket if queuer's status is queueing */}
+
       {/* Render allowed message if queuer's status is allowed */}
+
+      {queuer.status === "allowed" ? (
+        <p>You may now enter</p>
+      ) : (
+        <p>You shall not pass</p>
+      )}
+
       {/* Render allowed message if queuer's status is denied */}
+
       {/* Render not in queue message if queuer's status is idle */}
     </div>
   );
