a2df431c7949311c2a58da082ba1695d30519509
JokerStyllo <73703344+JokerStyllo@users.noreply.github.com>
2020-12-18 21:40:42 +0800

    k

--- src/components/DiscoverCard.js
+++ src/components/DiscoverCard.js
@@ -1,6 +1,13 @@
+import Discover from "./Discover";
+
 function DiscoverCard(props) {
   function enqueue() {
     // 1. Proceed if queuer's status is idle
+    {queuer.status === "idle" ? (
+      <p>hi</p>
+
+      
+    ) : <p>kk</p> }
     // 2. Add queuer to establishment's queue
     // 3. Set queuer's status to queueing
     // 4. Set queuer's queueingFor to props.id
