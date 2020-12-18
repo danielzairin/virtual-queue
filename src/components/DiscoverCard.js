99630bacc72bc1ca84911f7702bca074ae1ea90b
JokerStyllo <73703344+JokerStyllo@users.noreply.github.com>
2020-12-18 22:41:28 +0800

    k

--- src/components/DiscoverCard.js
+++ src/components/DiscoverCard.js
@@ -1,12 +1,18 @@
 import Discover from "./Discover";
+import { useEffect, useContext, useState } from "react";
+import { QueuerContext } from "../contexts/QueuerContext";
+import { db } from "../firebase";
 
 function DiscoverCard(props) {
+  const queuer = useContext(QueuerContext);
+  const [establishment, setEstablishment] = useState(null);
+  
   function enqueue() {
     // 1. Proceed if queuer's status is idle
     {queuer.status === "idle" ? (
       <p>hi</p>
 
-      
+
     ) : <p>kk</p> }
     // 2. Add queuer to establishment's queue
     // 3. Set queuer's status to queueing
