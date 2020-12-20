4a49c2f9b1ced035afb431436a1595ae1d0a0b30
JokerStyllo <73703344+JokerStyllo@users.noreply.github.com>
2020-12-20 17:57:22 +0800

    color

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,17 +2,17 @@ import { NavLink } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-sm bg-secondary navbar-dark fixed-bottom">
+    <nav className="navbar navbar-expand-xl bg-secondary navbar-dark fixed-bottom">
       {/*<h2>Navbar</h2>*/}
       <ul className="navbar-nav d-flex justify-content-around w-100">
-        <li className="nav-item"> 
-          <NavLink to="/discover">Discover</NavLink>
+        <li className="nav-item "> 
+          <NavLink className="text-decoration-none text-light" to="/discover">Discover</NavLink>
         </li>
         <li className="nav-item">
-          <NavLink to="/ticket">Ticket</NavLink>
+          <NavLink className="text-decoration-none text-light" to="/ticket">Ticket</NavLink>
         </li>
         <li className="nav-item">
-          <NavLink to="/manage">Manage</NavLink>
+          <NavLink className="text-decoration-none text-light" to="/manage">Manage</NavLink>
         </li>
       </ul>
     </nav>
